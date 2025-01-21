1d41ac452a71d67224a06823a17a079c86a78dd6
sherinemart <91425378+sherinemart@users.noreply.github.com>
2025-01-21 15:21:31 +0530

    Initial commit

---
+++ Arith.java
@@ -0,0 +1,23 @@
+class Arith
+{
+ public static void main(String args[])
+ {
+/*
+   int a,b,c,d;
+   a=b=c=d=20;
+   System.out.println("A: "+a);
+   System.out.println("B: "+b);
+   System.out.println("C: "+c);
+   System.out.println("D: "+d); 
+ 
+*/
+
+   byte b=30;
+  // b=b++;
+     b+=1;
+   System.out.println("B: "+b); 
+
+
+ }
+}
+   
